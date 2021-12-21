cbf01b849747a77eb73dffa1f2f38754fbeae0a6
damlacim <cim.damla@gmail.com>
2021-12-21 18:19:37 +0300

    create struct

--- API Request Tutorial/ViewController.swift
+++ API Request Tutorial/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2021-12-16"
+    
+    struct Response: Codable {
+        let results: MyResult
+        let status: String
+    }
+    
+    struct MyResult: Codable {
+        
+    }
 
 
 }
