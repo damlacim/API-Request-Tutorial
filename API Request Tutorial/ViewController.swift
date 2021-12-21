3e388d8d9c54251fb333ab11abcc6ebd44fdf1e8
damlacim <cim.damla@gmail.com>
2021-12-21 18:05:22 +0300

    Initial Commit

---
+++ API Request Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API Request Tutorial
+//
+//  Created by Damla Çim on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ API Request TutorialTests/API_Request_TutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  API_Request_TutorialTests.swift
+//  API Request TutorialTests
+//
+//  Created by Damla Çim on 21.12.2021.
+//
+
+import XCTest
+@testable import API_Request_Tutorial
+
+class API_Request_TutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
