dad7e25bc27662e3e87125d11ed523b5f0f5d7c3
damlacim <cim.damla@gmail.com>
2021-12-21 18:27:45 +0300

    add data

--- API Request Tutorial/ViewController.swift
+++ API Request Tutorial/ViewController.swift
@@ -22,9 +22,37 @@ class ViewController: UIViewController {
     }
     
     struct MyResult: Codable {
+        let sunrise: String
+        let sunset: String
+        let solar_noon: String
+        let day_lenght: String
+        let civil_twilight_begin: String
+        let civil_twilight_end: String
+        let nautical_twilight_begin: String
+        let nautical_twilight_end: String
+        let astronomical_twilight_begin: String
+        let astronomical_twilight_end: String
         
     }
 
 
 }
 
+/*
+ {
+       "results":
+       {
+         "sunrise":"2015-05-21T05:05:35+00:00",
+         "sunset":"2015-05-21T19:22:59+00:00",
+         "solar_noon":"2015-05-21T12:14:17+00:00",
+         "day_length":51444,
+         "civil_twilight_begin":"2015-05-21T04:36:17+00:00",
+         "civil_twilight_end":"2015-05-21T19:52:17+00:00",
+         "nautical_twilight_begin":"2015-05-21T04:00:13+00:00",
+         "nautical_twilight_end":"2015-05-21T20:28:21+00:00",
+         "astronomical_twilight_begin":"2015-05-21T03:20:49+00:00",
+         "astronomical_twilight_end":"2015-05-21T21:07:45+00:00"
+       },
+        "status":"OK"
+     }
+ */
